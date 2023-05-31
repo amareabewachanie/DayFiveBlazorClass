550b733db3081334295a5da06770fecbb02846da
Amare-PC <Amare-PC@DESKTOP-D74UL61>
2023-05-31 12:00:30 +0300

    Add project files.

---
+++ DayFiveBlazorClass/Migrations/20230531085630_SeedDataAdded.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DayFiveBlazorClass.Migrations
+{
+    public partial class SeedDataAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "City", "Country", "Name" },
+                values: new object[] { 1, "Addis Ababa", "Ethiopia", "Default Company" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
---
+++ DayFiveBlazorClass/Models/EmployeeManagmentContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DayFiveBlazorClass.Models
+{
+    public class EmployeeManagmentContext:DbContext
+    {
+        public EmployeeManagmentContext(DbContextOptions<EmployeeManagmentContext> opts):base(opts) 
+        {
+            
+        }
+        public DbSet<Company> Companies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Company>().HasData(
+                new Company()
+                {
+                    Id = 1,
+                    Name = "Default Company",
+                    City = "Addis Ababa",
+                    Country = "Ethiopia"
+                });
+        }
+    }
+}
